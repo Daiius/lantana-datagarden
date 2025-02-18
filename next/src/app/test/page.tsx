3b01a651bdb147723f3e205bc2d260ca3b7797e0
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-02-18 18:22:02 +0900

    データベース構造の検討とUIテスト
    1対1対応するデータはまとめてJSON型に、
    1対多対応するデータは切り出して別のJSON型に、
    列のグループ化を、ユーザが意識しない1対1対応付けレベルと
    ユーザが行うレベルに分割する

--- next/src/app/test/page.tsx
+++ next/src/app/test/page.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 
 import Header from '@/components/header/Header';
 import RealtimeProject from '@/components/project/RealtimeProject';
+import RelationsTable from '@/components/table/RelationsTable';
 
 export default async function Page() {
   const zeroId = '00000000-0000-0000-0000-000000000000' as const;
@@ -13,7 +14,12 @@ export default async function Page() {
       )}
     >
       <Header />
+      {/*
       <RealtimeProject className='p-4' projectId={zeroId} />
+      <div className='h-[50vh] overflow-y-auto'>
+      </div>
+      */}
+      <RelationsTable />
     </div>
   );
 }
