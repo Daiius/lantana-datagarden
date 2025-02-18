3ac3bcdf39f1ea990f44f64383f77ba08d763c01
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-02-19 05:14:55 +0900

    データ表示レイアウトの検討
    複数列を持つデータをまるごと1行増やす操作と、
    1対多対応を作る操作の切り替え方法等を検討

--- next/src/app/test/page.tsx
+++ next/src/app/test/page.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 
 import Header from '@/components/header/Header';
-import RealtimeProject from '@/components/project/RealtimeProject';
-import RelationsTable from '@/components/table/RelationsTable';
+import RelationsTableTest from '@/components/table/RelationsTableTest';
 
 export default async function Page() {
   const zeroId = '00000000-0000-0000-0000-000000000000' as const;
   
   return (
-    <div
-      className={clsx(
-      )}
-    >
-      <Header />
-      {/*
-      <RealtimeProject className='p-4' projectId={zeroId} />
-      <div className='h-[50vh] overflow-y-auto'>
-      </div>
-      */}
-      <RelationsTable />
+    <div >
+      <Header className='h-8'/>
+      <RelationsTableTest 
+        className='m-2 p-2 h-[calc(100vh-8rem)]'
+      />
     </div>
   );
 }
