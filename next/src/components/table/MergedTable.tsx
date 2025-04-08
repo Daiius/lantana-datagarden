a6401d48b9a9ff7eda8e254b2d26c9df2d9c2eef
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-04-09 00:13:13 +0900

    異なるcolumnGroupを同じflowStepにした際の2種類の表示をコンポーネント化

---
+++ next/src/components/table/MergedTable.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+
+import { ColumnGroupWithData } from '@/types';
+
+import { FlowStepProps } from '@/components/table/FlowStep';
+import TableGroup from '@/components/table/TableGroup';
+
+type MergedTableProps = 
+  FlowStepProps & {
+    className?: string;
+  };
+
+const MergedTable = ({
+  flowStep,
+  iflowStep,
+}: FlowStepProps) => {
+  const mergedTableGroupName = 
+    flowStep.columnGroups.map(columnGroup =>
+      columnGroup.name
+    ).join(' & ');
+  const mergedColumns = Array.from( 
+    new Map(
+      flowStep.columnGroups.flatMap(columnGroup =>
+        columnGroup.columns
+      ).map(column => [column.id, column])
+    ).values()
+  );
+  const mergedColumnGroup: ColumnGroupWithData
+  return (
+    <>
+      <div className='font-bold text-lg'>
+        {mergedTableGroupName}
+      </div>
+      <TableGroup
+        istep={iflowStep}
+        columnGroup={}
+      />
+    </>
+  );
+};
+
+export default MergedTable;
+
