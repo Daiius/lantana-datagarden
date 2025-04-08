de34a1b9185e5e15b1530f02ef0674353de78161
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-04-09 00:12:28 +0900

    vimの一時ファイルがhot reloadを引き起こさない様に設定を追加

--- next/next.config.ts
+++ next/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   basePath: '/lantana-datagarden',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        ...config.watchOptions,
+        ignored: ['**/*.sw[pon]', '**/.*.sw[pon]'],
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
