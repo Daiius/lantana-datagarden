530489549ffbb976b56c12aeb66d8dcf11324533
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-04-12 16:50:32 +0900

    table関連のコードを一部だけ整理
    - データ追加用イベントハンドラを分離
    - 最初のflowStepのTableの右端の列幅が広くなる問題を修正
      - 想定通りの効果が無かった last:mr-8 というスタイルが原因だった

--- next/src/components/table/Tables.tsx
+++ next/src/components/table/Tables.tsx
@@ -124,7 +124,6 @@ const Tables = ({
         <div key={iflowStep} className='flex flex-col gap-8'>
           {/* 同じstepに属するcolumnGroupを縦に重ねて表示する部分 */}
           <FlowStep
-            className='last:mr-8'
             flowStep={flowStep}
             iflowStep={iflowStep}
             projectId={projectId}
