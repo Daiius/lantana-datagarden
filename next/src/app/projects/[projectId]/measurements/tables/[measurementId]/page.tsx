95d36d783bde2fba564149ed553931ac78a6589d
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-04-22 16:42:26 +0900

    measurement関連のルートを追加中

---
+++ next/src/app/projects/[projectId]/measurements/tables/[measurementId]/page.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx';
+
+import Header from '@/components/header/Header';
+import MeasurementTable from '@/components/measurements/MeasurementTable';
+import { MeasurementSelectLink } from '@/components/measurements/MeasurementColumnGroupSelectLink';
+
+export default async function MeasurementTablePage(
+  { params }
+  : { params: Promise<{ projectId: string, measurementId: string }> }
+) {
+  const { projectId, measurementId } = await params;
+  return (
+    <div>
+      <Header className='h-[4rem]' projectId={projectId} />
+      <div className='h-[calc(100vh-4rem)] p-4'>
+        <MeasurementSelectLink
+          projectId={projectId}
+          defaultId={Number(measurementId)}
+        />
+      </div>
+    </div>
+  );
+}
+
