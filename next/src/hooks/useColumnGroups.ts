2af795adbfe370d23bd1a494ab1187b6e63fa42d
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-04-25 14:27:10 +0900

    ローカルstateとupdateイベント関連の実装漏れを修正

--- next/src/hooks/useColumnGroups.ts
+++ next/src/hooks/useColumnGroups.ts
@@ -1,39 +1,71 @@
+'use client' // for hooks
+
+import { useEffect, useState } from 'react';
+
 import { trpc } from '@/providers/TrpcProvider';
+import type { ColumnGroup } from '@/types';
+import { useDebouncedCallback } from 'use-debounce';
+import { DebounceTime } from '@/hooks/common';
 
 export const useColumnGroups = ({
   projectId
 }: {
   projectId: string;
 }) => {
-  const { data } = 
-    trpc.condition.columnGroup.list.useQuery({ projectId });
-  
-  const utils = trpc.useUtils();
+
+  const { 
+    data: fetchedData,
+    isLoading,
+  } = trpc.condition.columnGroup.list.useQuery(
+    { projectId },
+    //{ keepPreviousData: true },
+  );
+  const [data, setData] = useState<ColumnGroup[]>([]);
+  useEffect(() => {
+    if (fetchedData != null) {
+      setData(fetchedData);
+    }
+  }, [fetchedData]);
+
+  const { mutateAsync: updateDb } =
+    trpc.condition.columnGroup.update.useMutation();
+  const debouncedUpdateDb = useDebouncedCallback(
+    async (newData: ColumnGroup) => await updateDb(newData),
+    DebounceTime,
+  );
+  const update = async (newData: ColumnGroup) => {
+    setData(data.map(d => d.id === newData.id ? newData : d));
+    await debouncedUpdateDb(newData);
+  };
+  trpc.condition.columnGroup.onUpdate.useSubscription(
+    { projectId },
+    { onData: newData => setData(data.map(d => d.id === newData.id ? newData : d)) }
+  );
+
+  const { mutateAsync: add } =
+    trpc.condition.columnGroup.add.useMutation();
   trpc.condition.columnGroup.onAdd.useSubscription(
     { projectId },
     {
-      onData: newData => utils.condition.columnGroup.list.setData(
-        { projectId },
-        data == null
-        ? [newData]
-        : [...data, newData]
-      ),
+      onData: newData => setData([...data, newData]),
     }
   );
+
+  const { mutateAsync: remove } =
+    trpc.condition.columnGroup.remove.useMutation();
   trpc.condition.columnGroup.onRemove.useSubscription(
     { projectId },
     {
-      onData: info => utils.condition.columnGroup.list.setData(
-        { projectId },
-        data == null
-        ? []
-        : data.filter(d => d.id !== info.id)
-      ),
+      onData: info => setData(data.filter(d => d.id !== info.id)),
     }
   );
 
   return {
     data,
+    isLoading,
+    update,
+    add,
+    remove,
   };
 };
 
