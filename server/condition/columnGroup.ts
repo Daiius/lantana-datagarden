761d23877fdea99ac5d58eec9228bad8df62a774
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-04-25 14:28:18 +0900

    カスケードデータ取得に不要な部分を削除、イベント漏れを修正

--- server/condition/columnGroup.ts
+++ server/condition/columnGroup.ts
@@ -21,6 +21,14 @@ import { createSubscription } from '../lib/common';
 
 
 const selectSchema = createSelectSchema(columnGroups);
+const idsSchema = selectSchema.pick({
+  projectId: true,
+  id: true,
+});
+const projectIdSchema = selectSchema.pick({
+  projectId: true,
+});
+type ProjectId = z.infer<typeof projectIdSchema>;
 
 type ColumnGroup = z.infer<typeof selectSchema>;
 
@@ -28,22 +36,21 @@ type ColumnGroupEvents = {
   onAdd: ColumnGroup;
   onRemove: Pick<ColumnGroup, 'id'|'projectId'>;
   onUpdate:     ColumnGroup;
-  onUpdateList: ColumnGroup;
 }
 export const ee = mitt<ColumnGroupEvents>();
+
+const filter = (data: ProjectId, input: ProjectId) =>
+  data.projectId === input.projectId;
 
 export const columnGroupRouter = router({
   /**
    * 指定したidのカテゴリを取得します
    */
   get: publicProcedure
-    .input(selectSchema.pick({ 
-      id: true,
-      projectId: true,
-    }))
+    .input(idsSchema)
     .query(async ({ input }) => await get(input)),
   list: publicProcedure
-    .input(z.object({ projectId: z.string() }))
+    .input(projectIdSchema)
     .query(async ({ input }) => await list(input)),
   /**
    * 指定したidのカテゴリを更新します
@@ -53,58 +60,31 @@ export const columnGroupRouter = router({
     .input(selectSchema)
     .mutation(async ({ input }) => {
       const newColumnGroup = await update(input);
-      if (newColumnGroup == null) throw new Error(
-        `cannot find updated columnGroup, ${input.id}`
-      );
-      if (newColumnGroup == null) 
-        throw new Error('failed to get updated nested column group');
       ee.emit('onUpdate', newColumnGroup);
-      ee.emit('onUpdateList', newColumnGroup);
     }),
   /**
    * 指定したidのカテゴリが更新された際に発生するイベントです
    */
   onUpdate: publicProcedure
-    .input(selectSchema.pick({
-      id: true,
-      projectId: true
-    }))
-    .subscription(({ input }) =>
-      createSubscription({
+    .input(projectIdSchema)
+    .subscription(({ input }) => createSubscription({
         eventEmitter: ee,
         eventName: 'onUpdate',
-        filter: data => (
-             data.id        === input.id 
-          && data.projectId === input.projectId
-        ),
+        filter: data => filter(data, input),
       })
     ),
-  onUpdateList: publicProcedure
-    .input(selectSchema.pick({ projectId: true }))
-    .subscription(({ input }) => createSubscription({
-      eventEmitter: ee,
-      eventName: 'onUpdateList',
-      filter: data => data.projectId === input.projectId
-    })),
-  /**
-   * 新しいカテゴリを追加します
-   *
-   * 新しいデータが追加されると、
-   * onAddイベントが発行され、同じprojectIdに紐づけられた
-   * 一連のcategoriesを取得できます
-   */
   add: publicProcedure
-    .input(selectSchema.partial({ id: true }))
+    .input(selectSchema.omit({ id: true }))
     .mutation(async ({ input }) => {
       const newColumnGroup = await add(input);
       ee.emit('onAdd', newColumnGroup);
     }),
   onAdd: publicProcedure
-    .input(selectSchema.pick({ projectId: true }))
+    .input(projectIdSchema)
     .subscription(({ input }) => createSubscription({
       eventEmitter: ee,
       eventName: 'onAdd',
-      filter: data => data.projectId === input.projectId
+      filter: data => filter(data, input),
     })),
   /**
    * 指定したColumnGroupを削除します
@@ -112,24 +92,17 @@ export const columnGroupRouter = router({
    * 関連付けられたColumnsもDataも全て削除されます
    */
   remove: publicProcedure
-    .input(selectSchema.pick({
-      id: true,
-      projectId: true,
-    }))
+    .input(idsSchema)
     .mutation(async ({ input }) => {
       const removedIds = await remove(input);
       ee.emit('onRemove', removedIds);
     }) ,
   onRemove: publicProcedure
-    .input(selectSchema.pick({ projectId: true }))
-    .subscription(({ input }) =>
-      createSubscription({
+    .input(projectIdSchema)
+    .subscription(({ input }) => createSubscription({
         eventEmitter: ee,
         eventName: 'onRemove',
-        filter: data => (
-          data.projectId === input.projectId
-        ),
-      })
-    ),
+        filter: data => filter(data, input),
+    })),
 });
 
