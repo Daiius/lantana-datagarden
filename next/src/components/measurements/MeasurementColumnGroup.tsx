8cf6f1f5433bb5c2dea478010de5ef93f4aab492
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-05-02 02:54:20 +0900

    不要なnullチェック削除やコンポーネント定義簡略化などのリファクタリング

--- next/src/components/measurements/MeasurementColumnGroup.tsx
+++ next/src/components/measurements/MeasurementColumnGroup.tsx
@@ -27,52 +27,45 @@ const MeasurementColumnGroup = ({
   update,
   remove,
   className,
-}: MeasurementColumnGroupProps) => {
-
-  if (columnGroup == null) {
-    return <Skeleton />
-  }
-
-  return (
-    <div
-      className={clsx(
-        'rounded-lg border border-base-100',
-        'p-4',
-        'bg-base-200',
-        className,
-      )}
-    >
-      <div className='flex flex-row'>
-        <fieldset className='fieldset'>
-          <label className='fieldset-label'>
-            列グループ名:
-          </label>
-          <Input
-            value={columnGroup.name}
-            onChange={async newValue => {
-              await update(
-                { ...columnGroup, name: newValue as string }
-              );
-            }}
-          />
-        </fieldset>
-        <Button 
-          className='text-error ms-auto'
-          onClick={async () => await remove({ 
-            projectId: columnGroup.projectId,
-            id: columnGroup.id,
-          })}
-        >
-          <IconTrash />
-        </Button>
-      </div>
-      <MeasurementColumns 
-        projectId={columnGroup.projectId}
-        columnGroupId={columnGroup.id}
-      />
+}: MeasurementColumnGroupProps) => (
+  <div
+    className={clsx(
+      'rounded-lg border border-base-100',
+      'p-4',
+      'bg-base-200',
+      className,
+    )}
+  >
+    <div className='flex flex-row'>
+      <fieldset className='fieldset'>
+        <label className='fieldset-label'>
+          列グループ名:
+        </label>
+        <Input
+          value={columnGroup.name}
+          onChange={async newValue => {
+            await update(
+              { ...columnGroup, name: newValue as string }
+            );
+          }}
+        />
+      </fieldset>
+      <Button 
+        className='text-error ms-auto'
+        onClick={async () => await remove({ 
+          projectId: columnGroup.projectId,
+          id: columnGroup.id,
+        })}
+      >
+        <IconTrash />
+      </Button>
     </div>
-  );
-};
+    <MeasurementColumns 
+      projectId={columnGroup.projectId}
+      columnGroupId={columnGroup.id}
+    />
+  </div>
+);
 
 export default MeasurementColumnGroup;
 
