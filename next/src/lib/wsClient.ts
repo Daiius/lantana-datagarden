43390dc3d3d7435ee0442b184e106c991b810d4a
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-05-15 00:41:03 +0900

    tRPCコネクション数増加を抑える目的でシングルトンパターンを使用
    サーバ側でカウントしている接続数の際限ない増加がなくなった。
    それでも1クライアント当たり2接続で、もう少し調査が必要。

--- next/src/lib/wsClient.ts
+++ next/src/lib/wsClient.ts
@@ -1,6 +1,14 @@
 import { createWSClient } from '@trpc/client';
 
-export const wsClient = createWSClient({
-  url: `ws://${process.env.NEXT_PUBLIC_SERVER_IP}:3001`,
-});
+type WSClient = ReturnType<typeof createWSClient>;
+let wsClient: WSClient | null = null;
+
+export const getWSClient = () => {
+  if (wsClient == null) {
+    wsClient = createWSClient({
+      url: `ws://${process.env.NEXT_PUBLIC_SERVER_IP}:3001`,
+    });
+  }
+  return wsClient;
+}
 
