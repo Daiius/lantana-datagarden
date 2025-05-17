9b1c1d786a8c9d756274f1f785370f658c09a455
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-05-18 01:19:56 +0900

    onAdd時の実装ミス修正と小規模なリファクタリング

--- next/src/hooks/useColumnGroupMeasurements.ts
+++ next/src/hooks/useColumnGroupMeasurements.ts
@@ -7,6 +7,8 @@ import type { ColumnGroupMeasurement } from '@/types';
 import { useDebouncedCallback } from 'use-debounce';
 import { DebounceTime } from './common';
 
+import { log } from '@/debug';
+
 export type UseColumnGroupMeasurementsArgs = {
   projectId: string;
   columnGroupId: number;
@@ -16,20 +18,19 @@ export const useColumnGroupMeasurements = ({
   projectId,
   columnGroupId,
 }: UseColumnGroupMeasurementsArgs) => {
+  const target = trpc.condition.columnGroupMeasurement;
   const { 
     data: fetchedData 
-  } = trpc.condition.columnGroupMeasurement.list.useQuery({
-    columnGroupId, projectId
-  });
+  } = target.list.useQuery({ columnGroupId, projectId });
   const [data, setData] = useState<ColumnGroupMeasurement[]>([]);
   useEffect(() => {
     if (fetchedData != null) {
+      log('useColumnGroupMeasurements:useEffect updated from server data');
       setData(fetchedData);
     }
   }, [fetchedData]);
 
-  const { mutateAsync: updateDb } =
-    trpc.condition.columnGroupMeasurement.update.useMutation();
+  const { mutateAsync: updateDb } = target.update.useMutation();
   const debouncedUpdateDb = useDebouncedCallback(
     async (newValue: ColumnGroupMeasurement) => await updateDb(newValue),
     DebounceTime,
@@ -39,36 +40,19 @@ export const useColumnGroupMeasurements = ({
     await debouncedUpdateDb(newValue);
   };
 
-  trpc.condition.columnGroupMeasurement.onUpdate.useSubscription(
-    { projectId, columnGroupId },
-    {
-      onData: newData => setData(
-        data.map(d => d.id === newData.id ? newData : d)
-      ),
-    }
-  );
+  target.onUpdate.useSubscription({ projectId, columnGroupId }, {
+    onData: newData => setData(data.map(d => d.id === newData.id ? newData : d)),
+  });
 
-  const { mutateAsync: add } =
-    trpc.condition.columnGroupMeasurement.add.useMutation();
-  trpc.condition.columnGroupMeasurement.onAdd.useSubscription(
-    { projectId, columnGroupId },
-    { 
-      onData: newData => setData(
-        data.map(d => d.id === newData.id ? newData : d)
-      ), 
-    }
-  );
+  const { mutateAsync: add } = target.add.useMutation();
+  target.onAdd.useSubscription({ projectId, columnGroupId }, { 
+    onData: newData => setData([ ...data, newData]), 
+  });
 
-  const { mutateAsync: remove } =
-    trpc.condition.columnGroupMeasurement.remove.useMutation();
-  trpc.condition.columnGroupMeasurement.onRemove.useSubscription(
-    { projectId, columnGroupId },
-    {
-      onData: info => setData(
-        data.filter(d => d.id !== info.id)
-      ),
-    }
-  );
+  const { mutateAsync: remove } = target.remove.useMutation();
+  target.onRemove.useSubscription({ projectId, columnGroupId }, {
+    onData: info => setData(data.filter(d => d.id !== info.id)),
+  });
 
   return { 
     data,
